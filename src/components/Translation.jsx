becba48ebfa75e65f569ac5c606f4150728b767f
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2023-01-24 18:15:25 +0300

    Add files via upload

---
+++ src/components/Translation.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Translation({ doStuff, setInput, result }) {
+  return (
+    <div>
+      <textarea
+        className="text-area"
+        cols={55}
+        rows={10}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <br />
+      <button className="action-btn" onClick={doStuff}>
+        DO YOU STUFF!
+      </button>
+
+      <h3 className="result-text">{result.length > 0 ? result : ""}</h3>
+    </div>
+  );
+}
